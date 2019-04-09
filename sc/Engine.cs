4df9ff73843b4b4e5ca8dbc8b66a09c3b89f3786
Jason Cole <coleoid@yahoo.com>
2019-04-09 11:42:07 -0400

    Move Map to a window

--- sc/Engine.cs
+++ sc/Engine.cs
@@ -6,6 +6,7 @@ using Keys = Microsoft.Xna.Framework.Input.Keys;
 using log4net;
 using SadConsole.Components;
 using System;
+using SadConsole.Controls;
 
 namespace CbRework
 {
@@ -13,6 +14,7 @@ namespace CbRework
     public class Engine : ContainerConsole
     {
         private ILog log;
+        public Window MapWindow;
         private ScrollingConsole MapConsole;
         private SadConsole.Input.Keyboard Kbd;
         private Entity Player;
@@ -48,20 +50,51 @@ namespace CbRework
         {
             var gen = new MapGen();
             Map = gen.GenerateMap(MapWidth, MapHeight, 100, 5, 15);
-            
-            CreateConsoles();
-
             Player = CreatePlayer(Map.PlayerStartPoint);
 
-            MapConsole.Children.Add(Player);
+            CreateConsoles();
+            CreateMapWindow(WindowWidth / 2, WindowHeight / 2, "Game Map");
+
             MapConsole.CenterViewPortOnPoint(Player.Position);
         }
 
         public void CreateConsoles()
         {
             MapConsole = new ScrollingConsole(MapWidth, MapHeight, Global.FontDefault, new Rectangle(0, 0, WindowWidth, WindowHeight), Map.Tiles);
-            Children.Add(MapConsole);
             log.Debug("Created map console.");
+        }
+
+        public void CreateMapWindow(int width, int height, string title)
+        {
+            MapWindow = new Window(width, height)
+            {
+                CanDrag = true
+            };
+
+            // Fit the MapConsole into the window
+            int mapConsoleWidth = width - 2;
+            int mapConsoleHeight = height - 2;
+            MapConsole.ViewPort = new Rectangle(0, 0, mapConsoleWidth, mapConsoleHeight);
+            MapConsole.Position = new Point(1, 1);
+
+            //TODO: make click do something
+            Button closeButton = new Button(3, 1)
+            {
+                Position = new Point(width-3, 0),
+                Text = "[X]"
+            };
+            MapWindow.Add(closeButton);
+
+            // Centre the title text at the top of the window
+            MapWindow.Title = title.Align(HorizontalAlignment.Center, mapConsoleWidth);
+
+            //  These belong elsewhere.
+            Children.Add(MapWindow);
+            MapWindow.Children.Add(MapConsole);
+            MapConsole.Children.Add(Player);
+
+            MapWindow.Show();
+            log.Debug("Created map window.");
         }
 
         private Entity CreatePlayer(Point playerLocation)

e763b114bf2a7dc7751c0c324a9b4a94cd7983ad
Jason Cole <coleoid@yahoo.com>
2019-08-16 16:50:47 -0400

    Closer to custom unit test runner

---
+++ sc/GameRunningTestRunner.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Engine;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CopperBend.Application
+{
+    internal class GameRunningTestRunner : ITestEventListener
+    {
+        public List<string> EventReports { get; set; } = new List<string>();
+
+        public void RunTests()
+        {
+            ITestEngine engine = TestEngineActivator.CreateInstance();
+            TestPackage package = new TestPackage(@"sc.Tests.dll");
+            ITestRunner runner = engine.GetRunner(package);
+            XmlNode result = runner.Run(this, TestFilter.Empty);
+            WriteResult(result);
+        }
+
+        private static void WriteResult(XmlNode result)
+        {
+            var writer = new XmlTextWriter(Console.OpenStandardOutput(), Encoding.ASCII);
+            result.WriteTo(writer);
+            writer.Flush();
+            writer.Close();
+            Console.WriteLine();
+        }
+
+        public void OnTestEvent(string report)
+        {
+            EventReports.Add(report);
+        }
+    }
+}
--- sc/Program.cs
+++ sc/Program.cs
@@ -2,13 +2,47 @@
 using System.IO;
 using log4net;
 using log4net.Config;
+using McMaster.Extensions.CommandLineUtils;
+using NUnit.Engine;
 using Game = SadConsole.Game;
 
 namespace CopperBend.Application
 {
-    public static class Program
+    public class Program
     {
-        static void Main()
+        [Option("-t|--Test")]
+        public bool TestMode { get; }
+
+        // main() should boil down to:
+        //public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        public static int Main(string[] args)
+        {
+            return CommandLineApplication.Execute<Program>(args);
+            //var app = new CommandLineApplication();
+
+            //app.HelpOption();
+            //return app.Execute(args);
+        }
+
+        public void OnExecute()
+        {
+            if (TestMode)
+                RunTests();
+            else
+                RunGame();
+        }
+
+        public void RunTests()
+        {
+            Console.WriteLine("RunTests");
+            var runner = new GameRunningTestRunner();
+            runner.RunTests();
+            ITestEngine testEngine = TestEngineActivator.CreateInstance();
+
+        }
+
+        public void RunGame()
         {
             ILog log;
             var repo = LogManager.CreateRepository("CB");
@@ -22,7 +56,8 @@ namespace CopperBend.Application
 
             try
             {
-                Game.Create("Zaratustra.font", gameWidth, gameHeight);
+                //Game.Create("Zaratustra.font", gameWidth, gameHeight);
+                Game.Create(gameWidth, gameHeight);
 
                 //  Engine is now a console, which cannot be created before .Run() below.
                 //  .OnInitialize must be set before .Run is called.
@@ -37,5 +72,35 @@ namespace CopperBend.Application
             log.Info("Run ended");
             Game.Instance?.Dispose();
         }
+
+            //static void Main()
+            //{
+            //    ILog log;
+            //    var repo = LogManager.CreateRepository("CB");
+            //    XmlConfigurator.Configure(repo, new FileInfo("sc.log.config"));
+            //    log = LogManager.GetLogger("CB", "CB");
+            //    log.Info("\n======================================");
+            //    log.Info("Run started");
+
+            //    int gameWidth = 80;
+            //    int gameHeight = 60;
+
+            //    try
+            //    {
+            //        Game.Create("Zaratustra.font", gameWidth, gameHeight);
+
+            //        //  Engine is now a console, which cannot be created before .Run() below.
+            //        //  .OnInitialize must be set before .Run is called.
+            //        Game.OnInitialize = () => new Engine.Engine(gameWidth, gameHeight);
+            //        Game.Instance.Window.Title = "Copper Bend";
+            //        Game.Instance.Run();
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        log.Fatal("Exception terminated app", ex);
+            //    }
+            //    log.Info("Run ended");
+            //    Game.Instance?.Dispose();
+            //}
+        }
     }
-}

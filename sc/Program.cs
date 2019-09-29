5d0ad2676d03a402d2f27a5aeadc2369bf04a443
Coleoid <coleoid@yahoo.com>
2019-09-29 16:47:29 -0400

    Return to square font

--- sc/Program.cs
+++ sc/Program.cs
@@ -35,8 +35,8 @@ namespace CopperBend.Application
 
             try
             {
-                //Game.Create("Zaratustra.font", gameWidth, gameHeight);
-                Game.Create(gameWidth, gameHeight);
+                Game.Create("Zaratustra.font", gameWidth, gameHeight);
+                //Game.Create(gameWidth, gameHeight);
 
                 //  Engine is now a console, which cannot be created before .Run() below.
                 //  .OnInitialize must be set before .Run is called.
@@ -51,35 +51,5 @@ namespace CopperBend.Application
             log.Info("Run ended");
             Game.Instance?.Dispose();
         }
-
-            //static void Main()
-            //{
-            //    ILog log;
-            //    var repo = LogManager.CreateRepository("CB");
-            //    XmlConfigurator.Configure(repo, new FileInfo("sc.log.config"));
-            //    log = LogManager.GetLogger("CB", "CB");
-            //    log.Info("\n======================================");
-            //    log.Info("Run started");
-
-            //    int gameWidth = 80;
-            //    int gameHeight = 60;
-
-            //    try
-            //    {
-            //        Game.Create("Zaratustra.font", gameWidth, gameHeight);
-
-            //        //  Engine is now a console, which cannot be created before .Run() below.
-            //        //  .OnInitialize must be set before .Run is called.
-            //        Game.OnInitialize = () => new Engine.Engine(gameWidth, gameHeight);
-            //        Game.Instance.Window.Title = "Copper Bend";
-            //        Game.Instance.Run();
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        log.Fatal("Exception terminated app", ex);
-            //    }
-            //    log.Info("Run ended");
-            //    Game.Instance?.Dispose();
-            //}
-        }
     }
+}

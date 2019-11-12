205b0853c158fee25493faf5d4282a896346fc24
Jason Cole <coleoid@yahoo.com>
2019-11-12 14:35:16 -0500

    Clean UIBuilder, preparing to add status panel

--- sc/Engine/UIBuilder.cs
+++ sc/Engine/UIBuilder.cs
@@ -12,16 +12,15 @@ namespace CopperBend.Engine
 {
     public class UIBuilder
     {
-        private readonly ILog log;
         public readonly Size GameSize;
         //public Font Font;
+        private readonly ILog log;
 
         public UIBuilder(Size gameSize, Font font, ILog logger)
         {
-            log = logger;
-
             GameSize = gameSize;
             //Font = font;
+            log = logger;
         }
 
         public MessageLogWindow CreateMessageLog()
@@ -34,42 +33,16 @@ namespace CopperBend.Engine
             };
 
             ////  Garbage to visually test the window
-            //MessageLog.Add("Testing");
-            //MessageLog.Add("Testing B");
-            //MessageLog.Add("Testing three");
-            //MessageLog.Add("Testing 4");
-            //MessageLog.Add("Testing V");
-            //MessageLog.Add("Testing #6");
-            //MessageLog.Add("Testing Seventh");
-            //MessageLog.Add("Testing[7]");
+            //messageLog.Add("Testing");
+            //messageLog.Add("Testing B");
+            //messageLog.Add("Testing three");
+            //messageLog.Add("Testing 4");
+            //messageLog.Add("Testing V");
+            //messageLog.Add("Testing #6");
+            //messageLog.Add("Testing Seventh");
+            //messageLog.Add("Testing[7]");
 
             return messageLog;
-        }
-
-        public (LayeredConsole, Window) CreateMenuWindow(Size windowSize, string title)
-        {
-            int viewWidth = windowSize.Width - 2;
-            int viewHeight = windowSize.Height - 2;
-
-            Window menuWindow = new Window(windowSize.Width, windowSize.Height)
-            {
-                CanDrag = true,
-                Title = title.Align(HorizontalAlignment.Center, viewWidth)
-            };
-            log.DebugFormat("Created menu window, [{0}].", menuWindow.AbsoluteArea);
-
-            var menuConsole = new LayeredConsole(viewWidth, viewHeight, 2)
-            {
-                DefaultBackground = MG.Color.Black,
-            };
-
-            // Fit the Console inside the Window border
-            menuConsole.Position = new Coord(1, 1);
-            log.Debug("Created layered console.");
-
-            menuWindow.Children.Add(menuConsole);
-
-            return (menuConsole, menuWindow);
         }
 
         public (ControlsConsole, Window) CreateM2Window(Size windowSize, string title)
@@ -88,27 +61,15 @@ namespace CopperBend.Engine
             var menuConsole = new ControlsConsole(viewWidth, viewHeight)  //0.2 textier font
             {
                 DefaultBackground = MG.Color.Black,
-                //Theme = new SadConsole.Themes.Library {
-                //    ControlsConsoleTheme = new SadConsole.Themes.ControlsConsoleTheme(
-                //    new SadConsole.Themes.Colors()
-                //    {
-                //        // pick from a large selection here eventually...
-                //        // but this is not the rabbit to chase right now I think.
-                //    })
-                //}
             };
 
             // Fit the Console inside the Window border
             menuConsole.Position = new Coord(1, 1);
             log.DebugFormat("Created menu console, [{0}].", menuConsole.AbsoluteArea);
 
-            //menuConsole.Fill(MG.Color.White, MG.Color.Black, ' ');
             menuConsole.Clear();
-            //menuConsole.Print(2, 4, "R) Return to game");
-            //menuConsole.Print(2, 6, "Q) Quit");
 
             menuWindow.Children.Add(menuConsole);
-            //menuWindow.Hide();  // by default?
             return (menuConsole, menuWindow);
         }
 
@@ -123,14 +84,6 @@ namespace CopperBend.Engine
                 Title = title.Align(HorizontalAlignment.Center, viewWidth)
             };
             log.DebugFormat("Created map window, [{0}].", mapWindow.AbsoluteArea);
-
-            //// what would close button do, bring up save/quit?
-            //Button closeButton = new Button(3, 1)
-            //{
-            //    Position = new Coord(windowSize.Width - 3, 0),
-            //    Text = "X"
-            //};
-            //mapWindow.Add(closeButton);
 
             var mapConsole = new ScrollingConsole(
                 fullMap.Width, fullMap.Height, SadGlobe.FontDefault,

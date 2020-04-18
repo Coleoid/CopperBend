4e0f45edba5f7fa7ae65b18339a766e2e5223997
Jason Cole <coleoid@yahoo.com>
2020-04-18 11:55:41 -0400

    Improve IoC for Describer

--- sc/Contract/IDescriber.cs
+++ sc/Contract/IDescriber.cs
@@ -6,9 +6,6 @@ namespace CopperBend.Contract
     {
         string Describe(IItem item, DescMods mods = DescMods.None);
         string Describe(string name, DescMods mods = DescMods.None, int quantity = 1, string adj = "");
-        void Scramble();
-
-        //0.1 causes upward ref
-        TomeOfChaos TomeOfChaos { get; set; }
+        void Scramble(TomeOfChaos tome, Herbal herbal);
     }
 }
